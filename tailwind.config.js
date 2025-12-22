1442eaf8745fec767c2dd643520b667d2244258b
Kelokecl <contacto@keloke.cl>
2025-12-22 05:24:15 -0300

    Update Tailwind config for TypeScript support
    Added TypeScript and TSX file support in content array and defined primary and shopify colors in theme extension.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,jsx}',
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2D5016",
+        shopify: "#96BF48",
+      },
+    },
   },
   plugins: [],
-}
+};
