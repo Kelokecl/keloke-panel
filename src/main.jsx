63a1ad9b95e0e493a0a57012147a4c5411e11161
Kelokecl <contacto@keloke.cl>
2026-02-27 00:25:30 -0300

    Simplify rendering by removing React.StrictMode
    Removed React.StrictMode for simpler rendering.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// Nota: React.StrictMode en DEV puede duplicar ciertos efectos (doble mount).
+// En PROD no aplica el doble-invoke, pero para estabilidad durante debug
+// dejamos el render simple.
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
